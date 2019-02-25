9fbe97a4cee78fa6a8932478ec3fec2b393f0e32
Armughan-Shahid <marmughanshahid@gmail.com>
2019-02-25 12:17:15 +0500

    added prefix in squeezenet layers

--- detectron/modeling/SqueezeNet.py
+++ detectron/modeling/SqueezeNet.py
@@ -9,6 +9,7 @@ class SqueezeNet:
     self.data_format=1
     self.model=model
     self.output=self.create_model(Data)
+    self.prefix="gpu_0/"
 
   def fire_module(self,x,inp,fire_id,sq,exp_):
     s_id = 'fire' + str(fire_id) + '/'
@@ -18,17 +19,17 @@ class SqueezeNet:
     left=brew.relu(self.model,left,s_id + relu + exp1x1)
     right=brew.conv(self.model, x, s_id + exp3x3,sq, exp_, 3,legacy_pad=2)
     right=brew.relu(self.model,right,s_id + relu + exp3x3)
-    return brew.concat(self.model,[left,right],s_id + 'concat',axis=self.data_format),exp_*2
+    return brew.concat(self.model,[left,right],self.prefix+s_id + 'concat',axis=self.data_format),exp_*2
   def create_model(self,x):
-    x = brew.conv(self.model, x, 'conv1',3, 64, 3,stride=2)
-    x = brew.relu(self.model, x,'relu_conv1')
-    x = brew.max_pool(self.model, x, 'pool1', kernel=3, stride=2)
+    x = brew.conv(self.model, x, self.prefix+'conv1',3, 64, 3,stride=2)
+    x = brew.relu(self.model, x,self.prefix+'relu_conv1')
+    x = brew.max_pool(self.model, x, self.prefix+'pool1', kernel=3, stride=2)
     x,inp = self.fire_module(x, fire_id=2, sq=16, exp_=64,inp=64)
     x,inp = self.fire_module(x, fire_id=3, sq=16, exp_=64,inp=inp)
-    x = brew.max_pool(self.model, x, 'pool3', kernel=3, stride=2)
+    x = brew.max_pool(self.model, x, self.prefix+'pool3', kernel=3, stride=2)
     x,inp = self.fire_module(x, fire_id=4, sq=32, exp_=128,inp=inp)
     x,inp = self.fire_module(x, fire_id=5, sq=32, exp_=128,inp=inp)
-    x = brew.max_pool(self.model, x, 'pool5', kernel=3, stride=2)
+    x = brew.max_pool(self.model, x, self.prefix+'pool5', kernel=3, stride=2)
     x,inp = self.fire_module(x, fire_id=6, sq=48, exp_=192,inp=inp)
     x,inp = self.fire_module(x, fire_id=7, sq=48, exp_=192,inp=inp)
     x,inp = self.fire_module(x, fire_id=8, sq=64, exp_=256,inp=inp)
